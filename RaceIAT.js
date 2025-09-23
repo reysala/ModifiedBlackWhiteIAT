2823bb259fbdab35e9f0820f3d3d988f5024a78a
Adan <reysala@uni.edu>
2025-09-23 17:12:24 -0500

    Update RaceIAT.js

--- RaceIAT.js
+++ RaceIAT.js
@@ -1,4 +1,4 @@
-define(['pipAPI','https://github.com/reysala/ModifiedBlackWhiteIAT/ShortQualtricsIAT.js'], function(APIConstructor, iatExtension){
+define(['pipAPI','https://cdn.jsdelivr.net/gh/baranan/minno-tasks@0.*/IAT/qualtrics/quiat9.js'], function(APIConstructor, iatExtension){
 	var API = new APIConstructor();
   
   //Randomly choose the attribute words from a larger list of attribute words.
