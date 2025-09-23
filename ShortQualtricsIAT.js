c5e21eae900d9f27e5d9705ebb9e14c32551b920
Adan <reysala@uni.edu>
2025-09-23 17:22:17 -0500

    Update ShortQualtricsIAT.js

--- ShortQualtricsIAT.js
+++ ShortQualtricsIAT.js
@@ -120,9 +120,9 @@ define(['pipAPI','pipScorer','underscore'], function(APIConstructor, Scorer, _) 
 			////In each block, we can include a number of mini-blocks, to reduce repetition of same group/response.
 			////If you set the number of trials in any block to 0, that block will be skipped.
 			blockAttributes_nTrials : 0,
-			blockAttributes_nMiniBlocks : 5,
+			blockAttributes_nMiniBlocks : 0,
 			blockCategories_nTrials : 0,
-			blockCategories_nMiniBlocks : 5,
+			blockCategories_nMiniBlocks : 0,
 			blockFirstCombined_nTrials : 20,
 			blockFirstCombined_nMiniBlocks : 5,
 			blockSecondCombined_nTrials : 40, //Change to 0 if you want 5 blocks (you would probably want to increase blockFirstCombined_nTrials).
